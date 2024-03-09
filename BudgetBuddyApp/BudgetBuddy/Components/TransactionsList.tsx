c5345899109e34f7559486326534fc99453b3ea6
exec zsh <wind10cloud@gmail.com>
2024-03-09 10:08:28 +0900

    added card info

--- BudgetBuddyApp/BudgetBuddy/Components/TransactionsList.tsx
+++ BudgetBuddyApp/BudgetBuddy/Components/TransactionsList.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { Dispatch, SetStateAction } from 'react'
+import { Category, Transaction } from '../types'
+import TransactionListItem from './TransactionListItem'
 
-export default function TransactionsList() {
+export default function TransactionsList({
+  transactions, categories, deleteTransaction
+}: {
+  transactions: Transaction[],
+  categories: Category[],
+  deleteTransaction: (id: number) => Promise<void>
+}) {
 
 
   return (
     <View>
-      <Text>TransactionsList</Text>
+      {transactions.map((ts) => {
+        const categoryForCurrentItem = categories.find((ct) => ct.id === ts.category_id);
+
+        return (
+          <TouchableOpacity key={ts.id} activeOpacity={.7} onLongPress={() => deleteTransaction(ts.id)}>
+            <TransactionListItem transaction={ts} categoryInfo={categoryForCurrentItem} />
+          </TouchableOpacity>
+        )
+      })}
     </View>
   )
 }
--- BudgetBuddyApp/BudgetBuddy/Screens/Home.tsx
+++ BudgetBuddyApp/BudgetBuddy/Screens/Home.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Category, Transaction } from '../types';
 import { useSQLiteContext } from 'expo-sqlite/next';
+import TransactionsList from '../Components/TransactionsList';
 
 
 export default function Home() {
@@ -20,11 +21,22 @@ export default function Home() {
     const result = await db.getAllAsync<Transaction>(`SELECT * FROM Transactions ORDER BY date DESC;`);
     setTransactions(result);
     // console.log(result);
+
+    const categoryResult = await db.getAllAsync<Category>(`SELECT * FROM Categories;`);
+    setcategories(categoryResult);
+  }
+
+  async function deleteTransaction(id: number) {
+    db.withTransactionAsync(async () => {
+      await db.runAsync(`DELETE FROM Transactions WHERE id = ?;`, [id]);
+      await getData(); // reload the data from sqlite
+    })
+
   }
 
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ padding: 15, paddingVertical: 170 }}>
+      <TransactionsList categories={categories} transactions={transactions} deleteTransaction={deleteTransaction} />
+    </ScrollView>
   )
 }
